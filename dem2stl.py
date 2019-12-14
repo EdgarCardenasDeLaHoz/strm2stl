9224cb02f03a44d292d05ff72aae25e30b7da06e
Edgar Cardenas <Eac84.drexel@gmail.com>
2019-12-14 01:11:47 -0500

    add notebook with sandbox code and sample
    add notebook with sandbox code and sample

--- dem2stl.py
+++ dem2stl.py
@@ -6,7 +6,7 @@ import os.path
 from itertools import product
 
 import numpy as np
-
+from scipy.ndimage import rotate
 from PIL import Image
 
 
@@ -31,7 +31,7 @@ def get_section(root: str, ul: np.ndarray, lr: np.ndarray) -> np.ndarray:
         try:
             out[(x - ul[0]) * TILE_SHAPE[0]:(x - ul[0]+1) * TILE_SHAPE[0],
                 (y - ul[1]) * TILE_SHAPE[1]:(y - ul[1]+1) * TILE_SHAPE[1]
-            ] = np.array(Image.open(pathname))[:TILE_SHAPE[0], :TILE_SHAPE[1]]
+            ] = np.array(Image.open(pathname))[:TILE_SHAPE[0], :TILE_SHAPE[1]].T
         except FileNotFoundError:
             print(f"srtm_{x:02d}_{y:02d}.tif file not found")
             continue
@@ -39,4 +39,15 @@ def get_section(root: str, ul: np.ndarray, lr: np.ndarray) -> np.ndarray:
     return out.T
 
 
-
+def crop_region(DEM,ul,lr,theta):
+    """
+    Given rasterized mosaic DEM and rectangle bounded by grid
+    coordinates given by upper left coordinates `ul` and lower right
+    coordinates `lr` crop and rotate image 
+    """
+    if not theta==0:
+        DEM = rotate(DEM, theta, axes=(1, 0), reshape=True)
+    DEM = DEM[ul[0]:lr[0], ul[1]:lr[1]]
+    
+    
+    return DEM 
